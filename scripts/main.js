73db6be1ae3a50a50c9c53e41f0e003d3ae6f08f
bquoctruong <brianquochungtruong@gmail.com>
2018-02-28 13:51:38 -0800

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,80 @@
+var DETAIL_IMAGE_SELECTOR = "[data-image-role=\"target\"]";
+var DETAIL_TITLE_SELECTOR = "[data-image-role=\"title\"]";
+var THUMBNAIL_LINK_SELECTOR = "[data-image-role=\"trigger\"]";
+var LEFTCLICK_LINK_SELECTOR = "[data-image-role=\"trigger1\"]";
+var RIGHTCLICK_LINK_SELECTOR = "[data-image-role=\"trigger2\"]";
+
+var current = 2;
+//2/24 UPDATE: GET PREVIOUS CLICK TO WORK
+
+function setDetails(imageUrl, titleText){
+  "use strict";
+  var detailImage = document.querySelector(DETAIL_IMAGE_SELECTOR);
+  detailImage.setAttribute("src", imageUrl);
+
+  var detailTitle=document.querySelector(DETAIL_TITLE_SELECTOR);
+  detailTitle.textContent = titleText;
+}
+
+function imageFromThumb(thumbnail){
+  "use strict";
+  return thumbnail.getAttribute("data-image-url");
+}
+
+function titleFromThumb(thumbnail){
+  "use strict";
+  return thumbnail.getAttribute("data-image-title");
+}
+
+function setDetailsFromThumb(thumbnail){
+  "use strict";
+  setDetails(imageFromThumb(thumbnail), titleFromThumb(thumbnail));
+}
+
+function addThumbClickHandler(thumb){
+  "use strict";
+  thumb.addEventListener("click", function(event) {
+    event.preventDefault();
+    setDetailsFromThumb(thumb);
+  });
+}
+
+
+function getThumbnailsArray(){
+  "use strict";
+  var thumbnails = document.querySelectorAll(THUMBNAIL_LINK_SELECTOR);
+  var thumbnailArray = [].slice.call(thumbnails);
+  return thumbnailArray;
+}
+
+
+function initializeEvents(){
+  "use strict";
+  var thumbnails = getThumbnailsArray();
+  //var leftArrow = getPrevArrowArray();
+  thumbnails.forEach(addThumbClickHandler);
+  //leftArrow.forEach(addPrevArrowClickHandler);
+  var prev = document.querySelector(LEFTCLICK_LINK_SELECTOR);
+  var next = document.querySelector(RIGHTCLICK_LINK_SELECTOR);
+
+  prev.addEventListener("click", function(event){
+    event.preventDefault();
+    current -= 1;
+    if (current <= 0)
+    {
+      current = 6;
+    }
+    setDetailsFromThumb(thumbnails[current]);
+  });
+  next.addEventListener("click", function(event){
+    event.preventDefault();
+    current += 1;
+    if (current >= 6)
+    {
+      current = 0;
+    }
+    setDetailsFromThumb(thumbnails[current]);
+  });
+}
+
+initializeEvents();
